aed7e401d9422b11857ffbe7c053ff0b7428b2a0
yellow0125 <90314818+yellow0125@users.noreply.github.com>
2022-10-17 15:41:31 -0700

    navigation setoptions

--- myapp/App.js
+++ myapp/App.js
@@ -3,15 +3,29 @@ import Home from './components/Home'
 import GoalDetails from './components/GoalDetails'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Button } from 'react-native';
 
 const Stack = createNativeStackNavigator()
 
 export default function App() {
+  function rightButtonPressed(){
+    console.log('urgent')
+  }
+
+
+  function rightButton(){
+    return (
+    <Button 
+    title='urgent' 
+    onPress={rightButtonPressed}
+          
+  />)}
+    
   return (
     <NavigationContainer>
       <Stack.Navigator
         screenOptions={{
-          headerStyle: { backgroundColor: 'grey' },
+          headerStyle: { backgroundColor: 'purple' },
           headerTintColor: '#fff',
           headerTitleAlign: 'center'
         }}>
@@ -24,7 +38,8 @@ export default function App() {
         <Stack.Screen
           name='GoalDetails'
           component={GoalDetails}
-          options={({ route, navigation }) => {return { title: route.params.goalObj.text }}}
+          options={({ route, navigation }) => {return { title: route.params.goalObj.text,
+          headerRight:rightButton}}}
          />
       </Stack.Navigator>
     </NavigationContainer>
--- myapp/components/GoalDetails.js
+++ myapp/components/GoalDetails.js
@@ -1,7 +1,8 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function GoalDetails({route}) {
+export default function GoalDetails({route, navigation}) {
+  navigation.setOptions()
   return (
     <View>
       <Text>You are viewing details of {route.params.goalObj.text}</Text>
