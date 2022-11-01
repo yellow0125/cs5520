2e5a66bae2734f7f5f9f767d3d531259b038868f
yellow0125 <huang.cui@northeastern.edu>
2022-11-01 01:48:23 -0700

    update app.json

--- myapp/components/Header.js
+++ myapp/components/Header.js
@@ -1,13 +1,19 @@
 import { View, Text, StyleSheet, Dimensions, useWindowDimensions, Platform } from 'react-native'
 import React from 'react'
 
+//these two calues are not updated when the device is rotated
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
 export default function Header({ appName }) {
+
+  // these two values are updated as the orientation changes
+  const { width, height } = useWindowDimensions();
+  const paddingVerticalDyn = height < 415 ? 5 : 10; //landscape: 0 padding
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Welcome to {appName}!</Text>
+      <Text style={[styles.title, { paddingVertical: paddingVerticalDyn }]}>Welcome to {appName}!</Text>
     </View>
   )
 }
@@ -16,18 +22,19 @@ const styles = StyleSheet.create({
   container: {
     justifyContent: 'center',
     width: 350,
-    maxWidth: '90%'
+    maxWidth: '90%',
   },
   title: {
     color: 'rebeccapurple',
     fontWeight: 'bold',
     borderStyle: 'dotted',
     borderColor: 'slateblue',
-    borderWidth: Platform.select({ ios: 0, android: 8 }),
+    borderWidth: Platform.select({ ios: 0, android: 4 }),
     fontSize: windowWidth < 380 ? 18 : 22,
     paddingHorizontal: windowWidth < 380 ? 10 : 20,
     paddingVertical: 10,
     borderRadius: 5,
-    textAlign: 'center'
+    textAlign: 'center',
+    
   },
 });
