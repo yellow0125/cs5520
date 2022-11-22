dd7e9bdbe6050bc86e2b1ac19a6fb4c7d5caf05b
yellow0125 <huang.cui@northeastern.edu>
2022-11-22 00:46:35 -0800

    locate user with google map API

--- myapp/components/Profile.js
+++ myapp/components/Profile.js
@@ -1,11 +1,13 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { auth } from "../firebase/firebase-setup";
+import LocationManager from "./LocationManager";
 export default function () {
   return (
     <View>
       <Text>{auth.currentUser.email}</Text>
       <Text>{auth.currentUser.uid}</Text>
+      <LocationManager/>
     </View>
   );
 }
